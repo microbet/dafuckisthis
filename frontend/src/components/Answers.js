90b289f8c360e0d3c06f51fef059f7b1ca1ba98d
Jay Hirsch <jay.softwaredev@gmail.com>
2019-01-03 20:43:59 -0800

    added some untracked files I need

---
+++ frontend/src/components/Answers.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+
+class Answers extends Component {
+  constructor(props){
+    super(props);
+	this.state = {
+          commentBatch : '',
+          localTrigger : this.props.trigger,
+        }
+  }
+
+  componentDidMount() {
+    this.fetchData();
+    this.timer = setInterval(() => {
+        this.props.triggerAnswers();
+      }, 5000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
+  // get the 10 most recent answers- must also know the comment
+  // id so you can go back and get the 10 before that
+  fetchData() {
+ //   this.setState( { localTrigger : 0 } );
+    this.props.unTriggerAnswers();
+    let retArr = [];
+    fetch( this.props.DATA_URI + "/get_answers?imageId=" + this.props.imageId)
+      .then(response => response.json())
+      .then(data =>  {
+         data.forEach(function(element) {
+           retArr.push(element['answer']);
+         });
+         this.setState( { 
+           commentBatch : retArr,
+         })
+      })
+      .catch(error => {
+      this.setState({ error, isLoading: false});
+        console.log("error: ", error);
+      })
+  }
+
+
+  render() {
+    let display = [];
+    if (Array.isArray(this.state.commentBatch)) {
+      this.state.commentBatch.map((element, index) => {
+        display.push(<ul>{element}</ul>);
+      });
+
+        }
+	return(
+          <div>
+          {display}
+        {(this.props.trigger) ? this.fetchData() : this.props.unTriggerAnswers() }
+          </div>
+	);
+    }
+}
+
+export default Answers 
